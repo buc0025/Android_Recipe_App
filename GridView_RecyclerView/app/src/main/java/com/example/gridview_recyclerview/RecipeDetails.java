fafb1eae69a28e97c31dc5eeeb8b23e8cb2dd7ae
buc0025 <stanley.yang001@gmail.com>
2020-09-23 12:36:10 -0400

    Added switch to save and delete favorites

--- GridView_RecyclerView/app/src/main/java/com/example/gridview_recyclerview/RecipeDetails.java
+++ GridView_RecyclerView/app/src/main/java/com/example/gridview_recyclerview/RecipeDetails.java
@@ -3,10 +3,13 @@ package com.example.gridview_recyclerview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +18,7 @@ public class RecipeDetails extends AppCompatActivity {
     private TextView recipeName, recipeDescription, recipeCategory, recipeIngredients, recipeDirections;
     private ImageView recipeImage;
     private Button favBtn;
+    private Switch favSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +31,8 @@ public class RecipeDetails extends AppCompatActivity {
         recipeImage = (ImageView) findViewById(R.id.bookThumbnail);
         recipeIngredients = (TextView) findViewById(R.id.txtIngredients);
         recipeDirections = (TextView) findViewById(R.id.txtDirections);
-        favBtn = findViewById(R.id.favBtn);
+//        favBtn = findViewById(R.id.favBtn);
+        favSwitch = findViewById(R.id.favSwitch);
 
         //***********Receive data*************
         Intent intent = getIntent();
@@ -39,16 +44,43 @@ public class RecipeDetails extends AppCompatActivity {
         final String directions = intent.getExtras().getString("Directions");
         final int image = intent.getExtras().getInt("Thumbnail");
 
-        favBtn.setOnClickListener(new View.OnClickListener() {
+//        favBtn.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(RecipeDetails.this, "Saved to favorites", Toast.LENGTH_SHORT).show();
+//
+//                FoodItem foodItem = new FoodItem(id, title, category, description, ingredients, directions, image);
+//
+//                FavoritesManager favoritesManager = new FavoritesManager(RecipeDetails.this);
+//
+//                favoritesManager.saveFavorites(foodItem);
+//            }
+//        });
+
+//        final SharedPreferences sharedPreferences = getSharedPreferences("save", MODE_PRIVATE);
+//        favSwitch.setChecked(sharedPreferences.getBoolean("value", true));
+        favSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
-            public void onClick(View v) {
-                Toast.makeText(RecipeDetails.this, "Favorite button pressed", Toast.LENGTH_SHORT).show();
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
 
-                FoodItem foodItem = new FoodItem(id, title, category, description, ingredients, directions, image);
-
-                FavoritesManager favoritesManager = new FavoritesManager(RecipeDetails.this);
-
-                favoritesManager.saveFavorites(foodItem);
+                    Toast.makeText(RecipeDetails.this, "Saved to favorites", Toast.LENGTH_SHORT).show();
+                    FoodItem foodItem = new FoodItem(id,title,category,description,ingredients,directions,image);
+                    FavoritesManager favoritesManager = new FavoritesManager(RecipeDetails.this);
+                    favoritesManager.saveFavorites(foodItem);
+//                    SharedPreferences.Editor editor = getSharedPreferences("save", MODE_PRIVATE).edit();
+//                    editor.putBoolean("value", true);
+//                    editor.apply();
+                    favSwitch.setChecked(true);
+                } else {
+                    FoodItem foodItem = new FoodItem(id,title,category,description,ingredients,directions,image);
+                    FavoritesManager favoritesManager = new FavoritesManager(RecipeDetails.this);
+                    favoritesManager.removeFavorites(foodItem);
+//                    SharedPreferences.Editor editor = getSharedPreferences("save", MODE_PRIVATE).edit();
+//                    editor.putBoolean("value", false);
+//                    editor.apply();
+                    favSwitch.setChecked(false);
+                }
             }
         });
 
